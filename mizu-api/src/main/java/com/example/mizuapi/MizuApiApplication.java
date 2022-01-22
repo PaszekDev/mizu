0ab159eb4bbb9e15c05a8f78a157ee2b9694d73f
Dominik Paszek <127356@student.uni.opole.pl>
2022-01-22 20:51:34 +0100

    init

---
+++ mizu-api/src/main/java/com/example/mizuapi/MizuApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mizuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MizuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MizuApiApplication.class, args);
+    }
+
+}
---
+++ mizu-api/src/test/java/com/example/mizuapi/MizuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mizuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MizuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
