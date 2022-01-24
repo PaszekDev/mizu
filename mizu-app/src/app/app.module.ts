a340879ae207c6a0b332820e8c9f02e4ac683aa6
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2022-01-24 22:07:43 +0100

    move font-family to app component

--- mizu-app/src/app/app.module.ts
+++ mizu-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
