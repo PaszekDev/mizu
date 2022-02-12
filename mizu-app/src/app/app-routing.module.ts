13419e8f26d822abcaa303dca0a894a2f07e36fe
Grzegorz Dudek <grzegorzdudekdev@gmail.com>
2022-02-12 19:04:42 +0100

    group related components in folder

--- mizu-app/src/app/app-routing.module.ts
+++ mizu-app/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminComponent} from './components/admin/admin.component';
-import { ChangeEmailGridComponent } from './components/change-email-grid/change-email-grid.component';
-import { ChangePasswordGridComponent } from './components/change-password-grid/change-password-grid.component';
+import { ChangeEmailGridComponent } from './components/settingsView/change-email-grid/change-email-grid.component';
+import { ChangePasswordGridComponent } from './components/settingsView/change-password-grid/change-password-grid.component';
 import { EmployeeGridComponent } from './components/employee-grid/employee-grid.component';
-import { HelpGridComponent } from './components/help-grid/help-grid.component';
+import { HelpGridComponent } from './components/settingsView/help-grid/help-grid.component';
 import {HomeComponent} from './components/home/home.component';
 import { LoginHistoryComponent } from './components/login-history/login-history.component';
 import {LoginComponent} from './components/login/login.component';
 import {PermissionGridComponent} from './components/permission-grid/permission-grid.component';
-import { SettingsComponent } from './components/settings/settings.component';
+import { SettingsComponent } from './components/settingsView/settings/settings.component';
 import {StudentGridComponent} from './components/student-grid/student-grid.component';
-import { UserInformationGridComponent } from './components/user-information-grid/user-information-grid.component';
-import { UserPreferencesGridComponent } from './components/user-preferences-grid/user-preferences-grid.component';
+import { UserInformationGridComponent } from './components/settingsView/user-information-grid/user-information-grid.component';
+import { UserPreferencesGridComponent } from './components/settingsView/user-preferences-grid/user-preferences-grid.component';
 import {AuthenticationGuard} from './helpers/authentication.guard';
 
 const routes: Routes = [
--- mizu-app/src/app/app.module.ts
+++ mizu-app/src/app/app.module.ts
@@ -32,12 +32,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CustomizeTableDialogComponent } from './dialog/customize-table-dialog/customize-table-dialog.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { SettingsComponent } from './components/settings/settings.component';
-import { UserInformationGridComponent } from './components/user-information-grid/user-information-grid.component';
-import { ChangeEmailGridComponent } from './components/change-email-grid/change-email-grid.component';
-import { ChangePasswordGridComponent } from './components/change-password-grid/change-password-grid.component';
-import { UserPreferencesGridComponent } from './components/user-preferences-grid/user-preferences-grid.component';
-import { HelpGridComponent } from './components/help-grid/help-grid.component';
+import { SettingsComponent } from './components/settingsView/settings/settings.component';
+import { UserInformationGridComponent } from './components/settingsView/user-information-grid/user-information-grid.component';
+import { ChangeEmailGridComponent } from './components/settingsView/change-email-grid/change-email-grid.component';
+import { ChangePasswordGridComponent } from './components/settingsView/change-password-grid/change-password-grid.component';
+import { UserPreferencesGridComponent } from './components/settingsView/user-preferences-grid/user-preferences-grid.component';
+import { HelpGridComponent } from './components/settingsView/help-grid/help-grid.component';
 
 @NgModule({
   declarations: [
