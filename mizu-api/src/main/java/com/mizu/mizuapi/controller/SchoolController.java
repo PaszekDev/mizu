c4ec8ba116880242210b562ee7768d43810dd39b
Grzegorz Dudek <grzegorzdudekdev@gmail.com>
2022-02-25 12:47:59 +0100

    add CrossOrigin

--- mizu-api/src/main/java/com/mizu/mizuapi/controller/SchoolController.java
+++ mizu-api/src/main/java/com/mizu/mizuapi/controller/SchoolController.java
@@ -1,19 +1,18 @@
 package com.mizu.mizuapi.controller;
 
 import com.mizu.mizuapi.helper.MizuSchoolConnector;
+import com.mizu.mizuapi.request.search.SearchRequest;
 import com.mizu.mizuapi.response.EntityList;
 import com.mizu.mizuapi.ws.SchoolDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/school")
+@CrossOrigin(originPatterns = "*", maxAge = 3600)
 public class SchoolController {
     @Autowired
     MizuSchoolConnector mizuSchoolConnector;
@@ -28,5 +27,12 @@ public class SchoolController {
     @GetMapping
     public EntityList<SchoolDTO> getAll() {
         return (EntityList<SchoolDTO>) this.mizuSchoolConnector.getAll("/school", EntityList.class).block();
+    }
+
+    @DeleteMapping("{id}")
+    public SchoolDTO deleteById(@PathVariable Long id) {
+        Map<String, Long> params = new HashMap<>();
+        params.put("id", id);
+        return (SchoolDTO) this.mizuSchoolConnector.delete("/school/{id}", params, SchoolDTO.class).block();
     }
 }
